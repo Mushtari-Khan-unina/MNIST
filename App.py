40b36b7f1787f7a7b72ab6fa8f849be164661635
Mushtari-Khan-unina <143892251+Mushtari-Khan-unina@users.noreply.github.com>
2023-09-09 05:46:49 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,39 +1,74 @@
-# Import necessary libraries
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
-from tensorflow import keras
-import cv2
 import numpy as np
+import tensorflow as tf
+from PIL import Image
 
-# Load the model
-model_new = keras.models.load_model('/content/drive/MyDrive/MNIST digit classification/mnist.hdf5')
+# Load your pre-trained MNIST digit classification model
+model = tf.keras.models.load_model('your_model_directory')  # Replace with the actual path to your model
 
-# Define the Streamlit app
-st.title("MNIST Digit Recognizer")
-
-SIZE = 192
+# Set Streamlit app title and header
+st.title("MNIST Digit Classifier")
+st.header("Draw a digit and let the model classify it!")
 
 # Create a canvas for drawing
-canvas_result = st_canvas(
-    fill_color="#ffffff",
+canvas = st.canvas(
+    fill_color="black",
     stroke_width=10,
-    stroke_color='#ffffff',
-    background_color="#000000",
-    height=150,
-    width=150,
-    drawing_mode='freedraw',
-    key="canvas",
+    stroke_color="white",
+    background_color="black",
+    height=200,
+    width=200,
 )
 
-if canvas_result.image_data is not None:
-    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-    img_rescaling = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-    st.write('Input Image')
-    st.image(img_rescaling)
+# Function to preprocess and predict the digit
+def predict_digit(image):
+    # Resize the image to match the input size expected by the model (28x28 pixels)
+    image = image.resize((28, 28), Image.ANTIALIAS)
+    
+    # Convert the image to grayscale and normalize pixel values
+    image = np.array(image.convert('L')) / 255.0
+    
+    # Reshape the image to match the input shape expected by the model (1, 28, 28, 1)
+    image = np.reshape(image, (1, 28, 28, 1))
+    
+    # Make a prediction using the model
+    prediction = model.predict(image)
+    
+    # Get the predicted digit
+    digit = np.argmax(prediction)
+    
+    return digit, prediction
 
-if st.button('Predict'):
-    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    pred = model_new.predict(test_x.reshape(1, 28, 28, 1))
-    st.write(f'Result: {np.argmax(pred[0])}')
-    st.bar_chart(pred[0])
+# When the user clicks the "Predict" button
+if st.button("Predict"):
+    # Get the drawn image from the canvas
+    drawn_image = canvas.image
+    
+    # Check if the canvas is not empty
+    if drawn_image is not None:
+        # Predict the digit and get the confidence scores
+        predicted_digit, prediction_scores = predict_digit(drawn_image)
+        
+        # Display the predicted digit and confidence scores
+        st.write(f"Predicted Digit: {predicted_digit}")
+        st.write("Prediction Confidence:")
+        st.bar_chart(prediction_scores.flatten())
 
+# Add a reset button to clear the canvas
+if st.button("Clear Canvas"):
+    canvas.clear()
+
+# Add some additional information
+st.sidebar.header("About")
+st.sidebar.write("This app allows you to draw a digit on the canvas and uses a pre-trained model to classify it. It's based on the MNIST dataset, which contains handwritten digits (0-9).")
+
+# Add a GitHub link to your model repository
+st.sidebar.write("GitHub Repository:")
+st.sidebar.markdown("[Your Model Repository](https://github.com/your_username/your_model_repo)")
+
+# Add a footer with your name or credits
+st.footer("Developed by Your Name")
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    st.run()
