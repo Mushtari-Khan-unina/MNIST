a0f454bf6588e60127313f7472917e47315235a0
Mushtari-Khan-unina <143892251+Mushtari-Khan-unina@users.noreply.github.com>
2023-09-09 05:29:58 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,22 +1,23 @@
+# Import necessary libraries
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import tensorflow as tf
+from tensorflow import keras
 import cv2
 import numpy as np
 
 # Load the model
-model_new = tf.keras.models.load_model('mnist.h5')
+model_new = keras.models.load_model('/content/drive/MyDrive/MNIST digit classification/mnist.hdf5')
 
-# Streamlit app title and description
+# Define the Streamlit app
 st.title("MNIST Digit Recognizer")
-st.write("Draw a digit in the canvas and click 'Predict' to recognize it.")
 
-# Canvas settings
 SIZE = 192
+
+# Create a canvas for drawing
 canvas_result = st_canvas(
     fill_color="#ffffff",
     stroke_width=10,
-    stroke_color='#000000',
+    stroke_color='#ffffff',
     background_color="#000000",
     height=150,
     width=150,
@@ -28,13 +29,11 @@ if canvas_result.image_data is not None:
     img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
     img_rescaling = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
     st.write('Input Image')
-    st.image(img_rescaling, use_column_width=True)
+    st.image(img_rescaling)
 
 if st.button('Predict'):
-    if canvas_result.image_data is not None:
-        test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        pred = model_new.predict(test_x.reshape(1, 28, 28, 1))
-        st.write(f'Result: {np.argmax(pred[0])}')
-        st.bar_chart(pred[0])
-    else:
-        st.warning("Please draw a digit before predicting.")
+    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    pred = model_new.predict(test_x.reshape(1, 28, 28, 1))
+    st.write(f'Result: {np.argmax(pred[0])}')
+    st.bar_chart(pred[0])
+
